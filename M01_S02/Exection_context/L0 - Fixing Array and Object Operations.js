1d30e771c4ef5044ba0b337a38618cf118a33a90
unknown <m.umarfarooq1175@gmail.com>
2025-03-06 23:52:44 +0530

    commit

---
+++ M01_S02/Exection_context/L0 - Fixing Array and Object Operations.js
@@ -0,0 +1,42 @@
+const library = {
+
+    books: [{ title: "The Hobbit", author: "J.R.R. Tolkien", year: 1937 }],
+    
+    addBook: function(book) {
+    
+    if (!book.title || !book.author || !book.year) {
+    
+    console.log("Book information is incomplete.");
+    
+    return;
+    
+    }
+    this.books.push(book);
+    
+    },
+    
+    findBookByTitle: function(title) {
+    
+    let u=this.books.findIndex(book => book.title === title);
+    return (u===-1)?"Book not found":this.books[u]
+    
+    },
+    
+    removeBook: function(title) {
+    
+    const index = this.books.findIndex(book => book.title === title);
+    
+    if (index !== -1) {
+    
+    this.books.splice(index, 1);
+    
+    } else {
+    
+    console.log("Book not found.");
+    
+    } } };
+    
+    library.addBook({ author: "George Orwell", year: 1949 });
+    library.findBookByTitle('The Hobbit')
+    library.removeBook("Harry potter")
+    console.log(library.books.length);
---
+++ M01_S02/Exection_context/L0 - Understanding Global and Function Execution Context.js
@@ -0,0 +1,14 @@
+
+var age =22
+
+function displayAge(age){
+  console.log(age)
+}
+function changeAge() {
+    age =30
+    console.log(age)
+}
+
+
+displayAge(age)
+changeAge(age)
